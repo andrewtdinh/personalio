3ade6c3fcbd730861e4bbbc24271560fc94cac99
Andrew Dinh <andrewdinh74@yahoo.com>
2015-04-15 18:08:37 -0700

    update Personal Info Section

--- client/index.js
+++ client/index.js
@@ -4,13 +4,41 @@
 
 $(document).ready(init);
 
+var root, user, cash, folios;
+
 function init(){
+  root = new Firebase('https://personalio.firebaseio.com/');
+  user = root.child('user');
+  cash = root.child('cash');
+  folios = root.child('folios');
+  user.on('value', userChanged);
+  cash.on('value', balanceChanged);
+
+
   $('#update-account').click(updateAccount);
   $('#get-quote').click(getQuote);
 }
 
+function balanceChanged(snapshot){
+  var balance = snapshot.val();
+  console.log(snapshot.val());
+  $('#total-balance').text('Total Account Balance: ' + balance);
+}
+
+function userChanged(snapshot){
+  var name = snapshot.val();
+  console.log(snapshot.val());
+  $('#owner').text('Account Owner: ' + name);
+}
+
 function updateAccount(){
-  
+  var name = $('#user').val();
+  var balance = $('#balance').val();
+  user.set(name);
+  cash.set(balance);
+  $('#name-div').remove();
+  $('#balance-div').remove();
+  $('#update-account').remove();
 }
 
 function getQuote(){
