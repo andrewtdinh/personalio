51ae6a998b14468ffb064fbb15bfc0863677451d
Christian Richard <lifepupil@gmail.com>
2015-04-15 19:04:28 -0700

    Initial UI complete

--- client/index.js
+++ client/index.js
@@ -4,14 +4,36 @@
 
 $(document).ready(init);
 
+var root, user, cash, folios;
+
 function init(){
+  root = new Firebase('https://personalio.firebaseio.com/');
+  user = root.child('user');
+  cash = root.child('cash');
+  folios = root.child('folios');
+
+  // CLICK HANDLERS
   $('#update-account').click(updateAccount);
+  cash.on('value', balanceChanged);
+
   $('#get-quote').click(getQuote);
 }
 
-function updateAccount(){
-  
+
+function balanceChanged(snapshot){
+  var name = snapshot.val();
+  $('#header').text('Todo : ' + name);
 }
+
+function updateAccount(){
+  var name = $('#user').val();
+  var balance = $('#balance').val();
+  // $('#user').val('');
+  user.set(name);
+  cash.set(balance);
+}
+
+
 
 function getQuote(){
   var symbol = $('#symbol').val().toUpperCase();
